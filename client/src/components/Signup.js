af78674d7f495ae49befa8f0fc997426767b443b
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-06-27 13:20:42 +0300

    complete code grade challenge

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
+import React, { useEffect, useState } from "react";
 import * as yup from "yup";
 export const SignupForm = () => {
   const [customers, setCustomers] = useState([{}]);
@@ -53,7 +53,6 @@ export const SignupForm = () => {
 
   return (
     <div>
-      <h1>Customer sign up form</h1>
       <form onSubmit={formik.handleSubmit} style={{ margin: "30px" }}>
         <label htmlFor="email">Email Address</label>
         <br />
